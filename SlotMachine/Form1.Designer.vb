a153e8d7087e12ce685b0a915839a0540804cecb
Sam <cjsam6@yahoo.com>
2019-03-22 19:48:36 +0800

    first commit

---
+++ SlotMachine/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.chanceText = New System.Windows.Forms.Label()
+        Me.pullButton = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(25, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 149)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(193, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 149)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "2"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(381, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 149)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "3"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'chanceText
+        '
+        Me.chanceText.AutoSize = True
+        Me.chanceText.Location = New System.Drawing.Point(22, 337)
+        Me.chanceText.Name = "chanceText"
+        Me.chanceText.Size = New System.Drawing.Size(151, 17)
+        Me.chanceText.TabIndex = 3
+        Me.chanceText.Text = "Number of chance left:"
+        '
+        'pullButton
+        '
+        Me.pullButton.Location = New System.Drawing.Point(207, 224)
+        Me.pullButton.Name = "pullButton"
+        Me.pullButton.Size = New System.Drawing.Size(75, 23)
+        Me.pullButton.TabIndex = 5
+        Me.pullButton.Text = "Pull "
+        Me.pullButton.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label4.Location = New System.Drawing.Point(190, 337)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 17)
+        Me.Label4.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(545, 459)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.pullButton)
+        Me.Controls.Add(Me.chanceText)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents chanceText As Label
+    Friend WithEvents pullButton As Button
+    Friend WithEvents Label4 As Label
+End Class
---
+++ SlotMachine/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub pullButton_Click(sender As Object, e As EventArgs) Handles pullButton.Click
+        Randomize()
+        Label1.Text = Int((Rnd() * 9) + 1)
+        Label2.Text = Int((Rnd() * 9) + 1)
+        Label3.Text = Int((Rnd() * 9) + 1)
+        Label4.Text = Label4.Text - 1
+
+        If Label4.Text = 0 Then
+            MessageBox.Show("End of game. Thank you!", "Finish", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            If MessageBox.Show("End of game. Thank you!", "Finish", MessageBoxButtons.OK, MessageBoxIcon.Exclamation) = DialogResult.OK Then
+                Close()
+
+            End If
+        End If
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label4.Text = 3
+    End Sub
+
+End Class
